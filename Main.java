0a4a8d3772aa334dbd58adc4d3d100ff78c28054
mohammedFahad26 <fahadrock256@gmail.com>
2024-09-25 12:26:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,43 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        // CREATE 3 OBJECTS FOR BIRD CLASS
-
-        BIRD BIRD1 = new BIRD("PIGEON",11,true);
-        BIRD BIRD2 = new BIRD("SPARROW",9,true);
-        BIRD BIRD3 = new BIRD("PARROT",8,true);
-
-        // CALL METHOD TO EXECUTE THE BEHAVIOR OF AN OBJECT
-
-        BIRD1.fly();
-        BIRD1.eat();
+        System.out.println("HI");
+        System.out.println("this is Fahad from B89");
+        System.out.println("076 699 2403");
+        System.out.println("BCAS CAMPUS");
 
 
-        BIRD2.fly();
-        BIRD2.eat();
-
-        BIRD3.fly();
-        BIRD3.eat();
-
-        // CREATE 3 OBJECTS FOR BIRD CLASS
-
-        PowerRangers Pr1 = new PowerRangers("RED",true,"GUN");
-        PowerRangers Pr2 = new PowerRangers("BLUE",true,"SWORD");
-        PowerRangers Pr3 = new PowerRangers("YELLOW",true,"ARROW");
-
-        // CALL METHOD TO EXECUTE THE BEHAVIOR OF AN OBJECT
-        Pr1.spin();
-        Pr1.jump();
-        Pr1.punch();
-
-        Pr2.spin();
-        Pr2.jump();
-        Pr2.punch();
-
-        Pr3.spin();
-        Pr3.jump();
-        Pr3.punch();
-        
     }
-
 }
